bce88aeccf49df1701da37f2fbf3d54c10fe19b0
Gabriele91 <dbgabri@gmail.com>
2023-11-01 13:25:57 +0100

    Add main loop (from game_loop and rust-doom-demo)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,72 @@
 // Define
-mod wad;
 mod map;
 mod math;
+mod time;
+mod wad;
+mod window;
+use math::Vec2;
+// Using engine
+use window::{doom_loop, doom_window, DoomSurface};
+// Using
+use winit::{
+    dpi::{LogicalSize, PhysicalSize},
+    event::VirtualKeyCode,
+    event_loop::EventLoop,
+};
+use winit_input_helper::WinitInputHelper;
 
 fn main() {
     if let Some(doom1) = wad::Reader::new(String::from("assets/DOOM1.WAD")) {
-        if let Some(map) = map::Map::new(&doom1, &String::from("E1M1")) {  
+        if let Some(map) = map::Map::new(&doom1, &String::from("E1M1")) {
             println!("{:?}", map);
         }
     }
+
+    let event_loop = EventLoop::new();
+    let window = doom_window(
+        String::from("Doom"),
+        LogicalSize::<f64>::new(320.0 * 4., 240.0 * 4.),
+        &event_loop,
+    )
+    .unwrap();
+    let context = (
+        DoomSurface::new(PhysicalSize::<u32>::new(320, 240), &window).unwrap(),
+        WinitInputHelper::new(),
+    );
+
+    doom_loop(
+        event_loop,
+        window,
+        context,
+        60,
+        0.0166666666666667,
+        |_| {
+            // Void
+        },
+        |dl| {
+            dl.context.0.clear([0, 0, 0, 0]);
+            for g in 0..360 {
+                let x = math::SIN[g] * 50.0;
+                let y = math::COS[g] * 50.0;
+                let xoffset = 160.0 + math::SIN[(dl.running_time * 500.0) as usize % 360] * 25.0;
+                let yoffset = 120.0 + math::COS[(dl.running_time * 100.0) as usize % 360] * 25.0;
+                dl.context.0.draw(
+                    &Vec2::new((x + xoffset) as usize, (y + yoffset) as usize),
+                    &[0xFF, 0, 0, 0xFF],
+                );
+            }
+            dl.context.0.swap().unwrap();
+        },
+        |dl, event| {
+            // Input
+            if dl.context.1.update(&event) {
+                // Close events
+                if dl.context.1.key_pressed(VirtualKeyCode::Escape)
+                    || dl.context.1.close_requested()
+                {
+                    dl.exit();
+                }
+            }
+        },
+    );
 }
---
+++ src/window.rs
@@ -0,0 +1,255 @@
+#![allow(dead_code)]
+use std::sync::Arc;
+// Using, d3d
+use crate::math::Vec2;
+use crate::time::{
+    TimeTrait,
+    Time
+};
+// Using
+use readonly;
+use pixels::{
+    Pixels, 
+    SurfaceTexture, 
+    Error
+};
+use winit::{
+    dpi::{LogicalSize, PhysicalSize},
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+pub struct DoomSurface {
+    pub size: PhysicalSize<u32>,
+    pub pixels: Pixels,
+}
+
+#[readonly::make]
+pub struct DoomLoopState<C, T: TimeTrait, W> {
+    pub context: C,
+    #[readonly]
+    pub(super) updates_per_second: u32,
+    #[readonly]
+    pub(super) max_frame_time: f64,
+    #[readonly]
+    pub(super) exit_next_iteration: bool,
+    #[readonly]
+    pub(super) window: W,
+    #[readonly]
+    pub(super) window_occluded: bool,
+    #[readonly]
+    pub(super) fixed_time_step: f64,
+    #[readonly]
+    pub(super) number_of_updates: u32,
+    #[readonly]
+    pub(super) number_of_renders: u32,
+    #[readonly]
+    pub(super) last_frame_time: f64,
+    #[readonly]
+    pub(super) running_time: f64,
+    #[readonly]
+    pub(super) accumulated_time: f64,
+    #[readonly]
+    pub(super) blending_factor: f64,
+    #[readonly]
+    pub(super) previous_instant: T,
+    #[readonly]
+    pub(super) current_instant: T,
+}
+
+impl DoomSurface {
+    pub fn new(size: PhysicalSize<u32>, window: &Window) -> Option<Self> {
+        let surface = SurfaceTexture::new(size.width, size.height, &window);
+        if let Ok(pixels) = Pixels::new(size.width, size.height, surface) {
+            Some(DoomSurface { size, pixels })
+        } else {
+            None
+        }
+    }
+
+    pub fn clear(&mut self, color: [u8; 4]) {
+        let frame = self.pixels.frame_mut();
+        let size = (frame.len() / color.len()) as usize;
+        frame.copy_from_slice(&color.repeat(size));
+    }
+
+    pub fn swap(&mut self) -> Result<(), Error> {
+        self.pixels.render()
+    }
+
+    pub fn draw(&mut self, position: &Vec2<usize>, color: &[u8]) {
+        let size = self.pixels.texture().size();
+        let channels = self.pixels.texture().format().block_size(None).unwrap() as usize;
+        if position.x >= size.width as usize || position.y >= size.height as usize {
+            return;
+        }
+        let frame = self.pixels.frame_mut();
+        let row_size = (size.width as usize) * channels; // 4 colors per byte
+        let offset: usize =
+            (size.height as usize - position.y - 1) * row_size + position.x * channels;
+        let mut ptr = frame.as_mut_ptr();
+        unsafe {
+            ptr = ptr.add(offset);
+            for channel in color.iter() {
+                (*ptr) = *channel;
+                ptr = ptr.add(1);
+            }
+        }
+    }
+}
+
+impl<C, T: TimeTrait, W> DoomLoopState<C, T, W> {
+    pub fn new(context: C, updates_per_second: u32, max_frame_time: f64, window: W) -> Self {
+        Self {
+            context,
+            updates_per_second,
+            max_frame_time,
+            window,
+            window_occluded: false,
+            exit_next_iteration: false,
+
+            fixed_time_step: 1.0 / updates_per_second as f64,
+            number_of_updates: 0,
+            number_of_renders: 0,
+            running_time: 0.0,
+            accumulated_time: 0.0,
+            blending_factor: 0.0,
+            previous_instant: T::now(),
+            current_instant: T::now(),
+            last_frame_time: 0.0,
+        }
+    }
+
+    pub fn next_frame<U, R>(&mut self, mut update: U, mut render: R) -> bool
+    where
+        U: FnMut(&mut DoomLoopState<C, T, W>),
+        R: FnMut(&mut DoomLoopState<C, T, W>),
+    {
+        let g = self;
+
+        if g.exit_next_iteration {
+            return false;
+        }
+
+        g.current_instant = T::now();
+
+        let mut elapsed = g.current_instant.sub(&g.previous_instant);
+        if elapsed > g.max_frame_time {
+            elapsed = g.max_frame_time;
+        }
+
+        g.last_frame_time = elapsed;
+        g.running_time += elapsed;
+        g.accumulated_time += elapsed;
+
+        while g.accumulated_time >= g.fixed_time_step {
+            update(g);
+
+            g.accumulated_time -= g.fixed_time_step;
+            g.number_of_updates += 1;
+        }
+
+        g.blending_factor = g.accumulated_time / g.fixed_time_step;
+
+        if g.window_occluded && T::supports_sleep() {
+            T::sleep(g.fixed_time_step);
+        } else {
+            render(g);
+            g.number_of_renders += 1;
+        }
+
+        g.previous_instant = g.current_instant;
+
+        true
+    }
+
+    pub fn re_accumulate(&mut self) {
+        let g = self;
+
+        g.current_instant = T::now();
+
+        let prev_elapsed = g.last_frame_time;
+        let new_elapsed = g.current_instant.sub(&g.previous_instant);
+
+        let delta = new_elapsed - prev_elapsed;
+
+        // We don't update g.last_frame_time since this additional time in the
+        // render function is considered part of the current frame.
+
+        g.running_time += delta;
+        g.accumulated_time += delta;
+
+        g.blending_factor = g.accumulated_time / g.fixed_time_step;
+    }
+
+    pub fn exit(&mut self) {
+        self.exit_next_iteration = true;
+    }
+
+    pub fn set_updates_per_second(&mut self, new_updates_per_second: u32) {
+        self.updates_per_second = new_updates_per_second;
+        self.fixed_time_step = 1.0 / new_updates_per_second as f64;
+    }
+}
+
+pub fn doom_window<T: 'static>(
+    title: String,
+    size: LogicalSize<f64>,
+    events: &EventLoop<T>,
+) -> Option<Arc<Window>> {
+    if let Ok(window) = WindowBuilder::new()
+        .with_title(title)
+        .with_inner_size(size.clone())
+        .with_min_inner_size(size.clone())
+        .build(&events)
+    {
+        Some(Arc::new(window))
+    } else {
+        None
+    }
+}
+
+pub fn doom_loop<C, U, R, H, T>(
+    event_loop: EventLoop<T>,
+    window: Arc<Window>,
+    context: C,
+    updates_per_second: u32,
+    max_frame_time: f64,
+    mut update: U,
+    mut render: R,
+    mut handler: H,
+) -> !
+where
+    C: 'static,
+    U: FnMut(&mut DoomLoopState<C, Time, Arc<Window>>) + 'static,
+    R: FnMut(&mut DoomLoopState<C, Time, Arc<Window>>) + 'static,
+    H: FnMut(&mut DoomLoopState<C, Time, Arc<Window>>, &Event<'_, T>) + 'static,
+    T: 'static,
+{
+    let mut doom_loop_state = DoomLoopState::new(context, updates_per_second, max_frame_time, window);
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        // Forward events to existing handlers.
+        handler(&mut doom_loop_state, &event);
+
+        match event {
+            Event::RedrawRequested(_) => {
+                if !doom_loop_state.next_frame(&mut update, &mut render) {
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            Event::MainEventsCleared => {
+                doom_loop_state.window.request_redraw();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Occluded(occluded),
+                ..
+            } => {
+                doom_loop_state.window_occluded = occluded;
+            }
+            _ => {}
+        }
+    })
+}
