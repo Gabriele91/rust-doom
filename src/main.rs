c5487307290769d96724b6083d1b85df6ea9a797
Gabriele91 <dbgabri@gmail.com>
2024-12-27 01:25:42 +0100

    Add cli

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,43 @@ use doom::Doom;
 use window::{doom_loop, doom_window};
 // Using
 use winit::event_loop::EventLoop;
+use std::env;
+use std::process::ExitCode;
 
-fn main() {
-    let configure = Configure::load_from_file(String::from("assets/doom.ini")).unwrap();
+fn cli_handler() -> Result<String, String> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {
+            Err(format!(
+                "Error: No arguments provided.\nUsage: {} <config-file>",
+                args[0]
+            ))
+        }
+        2 => {
+            let config_file = &args[1];
+            Ok(config_file.clone())
+        }
+        _ => {
+            Err(format!(
+                "Error: Too many arguments.\nUsage: {} <config-file>",
+                args[0]
+            ))
+        }
+    }
+}
+
+fn main() -> ExitCode {
+    let config_file = {
+        match cli_handler() {
+            Ok(config_file) => config_file,
+            Err(err) => { 
+                eprintln!("{}", err); 
+                return ExitCode::FAILURE;
+            }
+        }
+    };
+    let configure = Configure::load_from_file(config_file).unwrap();
     let event_loop = EventLoop::new().unwrap();
     let window = doom_window(
         &configure.screen.title,
@@ -32,4 +66,5 @@ fn main() {
     let doom = Doom::new(&window, &configure);
 
     make_doom_loop!(event_loop, window, doom, configure.screen.frame_rate).unwrap();
+    return ExitCode::SUCCESS;
 }
